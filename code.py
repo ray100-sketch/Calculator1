9021f052f49368a472d39820d80b4bfd0dc42be8
ray100-sketch <110620704+ray100-sketch@users.noreply.github.com>
2023-08-11 07:33:04 -0400

    code.py

---
+++ code.py
@@ -0,0 +1,24 @@
+
+operation = input("Enter Operation Type:")
+num1 = float(input("Enter First Number Here")
+num2 = float(input("Enter Second Number Here")
+
+if operation == '*':
+             print(num1 * num2)
+
+elif operation == '+':
+             print(num1 + num2)
+
+elif operation == '-':
+             print(num1 - num2)
+
+elif operation == '/':
+             print(num1 / num2)
+
+else:
+    print("Sorry Your Calculation Is Invalid.")
+
+
+
+
+  
